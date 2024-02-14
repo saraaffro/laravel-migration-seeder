2b7dbfa1c02c6227dbd9dbbf391dceb611428c50
saraaffro <saraaffro@gmail.com>
2024-02-14 10:10:34 +0100

    fix: syntax data

--- database/migrations/2024_02_13_115044_create_trains_table.php
+++ database/migrations/2024_02_13_115044_create_trains_table.php
@@ -14,17 +14,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
-            $table->id();
-            $table->string('company');
-            $table->string('departure_station');
-            $table->string('arrival_station');
-            $table->dateTime('departure_time');
-            $table->dateTime('arrival_time');
-            $table->string('train_code') -> unique();
-            $table->integer('carriages_count');
-            $table->boolean('on_time')->default(true);
-            $table->boolean('cancelled')->default(false);
-            $table->timestamps();
+            $table -> id();
+            $table -> string('company', 60);
+            $table -> string('departure_station', 60);
+            $table -> string('arrival_station', 60);
+            $table -> dateTime('departure_time');
+            $table -> dateTime('arrival_time');
+            $table -> string('train_code') -> unique();
+            $table -> integer('carriages_count');
+            $table -> boolean('on_time') -> default(true);
+            $table -> boolean('cancelled') -> default(false);
+            $table -> timestamps();
         });
     }
 
